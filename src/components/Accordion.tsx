be6553f8f94254ed86f1ac9bd37527aacc6fef19
Len Lester <lenster921@gmail.com>
2023-06-30 15:12:15 -0700

    fixed accordion

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -39,29 +39,8 @@ const Accordion: FC<AccordionProps> = ({ children }) => {
   )
 }
 
-const AccordionItem: FC<AccordionItemProps> = ({ isActive, onClick, children }) => {
-  const headerChild = React.Children.toArray(children).find((child) => {
-    return React.isValidElement(child) && (child.type as any)?.name === 'AccordionHeader'
-  }) as React.ReactElement<AccordionHeaderProps> | undefined
-
-  const bodyChildren = React.Children.toArray(children).filter((child) => {
-    return React.isValidElement(child) && (child.type as any)?.name === 'AccordionBody'
-  })
-
-  return (
-    <div className="rounded p-2 monokai-bg-black-2 shadow-lg">
-      <div className="flex items-center justify-between cursor-pointer" onClick={onClick}>
-        {headerChild}
-        {isActive ? <FiChevronUp className="ms-2 w-5 h-5" /> : <FiChevronDown className="ms-2 w-5 h-5" />}
-      </div>
-      {React.Children.map(bodyChildren, (child) =>
-        React.cloneElement(child as React.ReactElement<AccordionBodyProps>, { isActive }),
-      )}
-    </div>
-  )
-}
-
 const AccordionHeader: FC<AccordionHeaderProps> = ({ children }) => <h3 className="w-full">{children}</h3>
+AccordionHeader.displayName = 'AccordionHeader'
 
 const AccordionBody: FC<AccordionBodyProps> = ({ isActive, children }) => {
   const bodyRef = React.useRef<HTMLDivElement>(null)
@@ -77,6 +56,29 @@ const AccordionBody: FC<AccordionBodyProps> = ({ isActive, children }) => {
       style={{ maxHeight: isActive ? `${maxHeight}px` : 0 }}
     >
       {children}
+    </div>
+  )
+}
+AccordionBody.displayName = 'AccordionBody'
+
+const AccordionItem: FC<AccordionItemProps> = ({ isActive, onClick, children }) => {
+  const headerChild = React.Children.toArray(children).find((child) => {
+    return React.isValidElement(child) && (child.type as React.ComponentType)?.displayName === 'AccordionHeader'
+  }) as React.ReactElement<AccordionHeaderProps> | undefined
+
+  const bodyChildren = React.Children.toArray(children).filter((child) => {
+    return React.isValidElement(child) && (child.type as React.ComponentType)?.displayName === 'AccordionBody'
+  })
+
+  return (
+    <div className="rounded p-2 monokai-bg-black-2 shadow-lg">
+      <div className="flex items-center justify-between cursor-pointer" onClick={() => onClick?.()}>
+        {headerChild}
+        {isActive ? <FiChevronUp className="ms-2 w-5 h-5" /> : <FiChevronDown className="ms-2 w-5 h-5" />}
+      </div>
+      {React.Children.map(bodyChildren, (child) =>
+        React.cloneElement(child as React.ReactElement<AccordionBodyProps>, { isActive }),
+      )}
     </div>
   )
 }
